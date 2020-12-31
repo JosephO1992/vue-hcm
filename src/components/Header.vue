75996ecc5a960994ae1f7e899d51e97d0c16b37c
Joe <josephraoreilly@gmail.com>
2020-12-31 15:55:43 +0800

    removed unused files, added inital component

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Human Capital Management App.</h1>
+    <p>The one stop shop for all your staff tracking needs!</p>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
